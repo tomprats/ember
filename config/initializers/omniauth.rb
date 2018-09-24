6647bf67799a08da550704b06d554bb4bfcee57f
Tom Prats <tprats108@gmail.com>
2018-09-24 01:11:10 -0400

    Updated Facebook auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV["FACEBOOK_KEY"], ENV["FACEBOOK_SECRET"],
-    scope: "email, public_profile, user_likes, user_birthday, user_friends, user_location, user_about_me, user_relationship_details",
+    scope: "email, public_profile, user_likes, user_birthday, user_friends, user_location",
     info_fields: "email, first_name, last_name, link, bio, gender, interested_in, location, birthday"
 end
